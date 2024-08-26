f5055f29f22aa53381a4a6cf94c2ba15c3b52733
Sanjana N <159764453+Sanjana615@users.noreply.github.com>
2024-08-26 06:45:03 +0530

    Update alu_environment.sv

--- verification_codes/alu_environment.sv
+++ verification_codes/alu_environment.sv
@@ -8,13 +8,47 @@
  // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
  //------------------------------------------------------------------------------
  
-class alu_environment extends uvm_environment;
-    `uvm_uvm_component_utils(alu_environment)
 
-    function new(string name = "alu_environment",uvm_component parent=null);
-        super.new(name,parent);
-    endfunction
+class alu_environment extends uvm_env;
+ `uvm_component_utils(alu_environment)
+    
+    // Components
+    alu_agent agnt;
+    alu_scoreboard scb;
+    alu_fun_cov fc;
 
-    // Additional implementation details for uvm_component go here
+    // Constructor:new
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction //new()
 
-endclass
+    //  Function: build_phase
+    extern function void build_phase(uvm_phase phase);
+    
+    //  Function: connect_phase
+    extern function void connect_phase(uvm_phase phase);
+
+    //  Function: end_of_elaboration_phase
+    extern function void end_of_elaboration_phase(uvm_phase phase);  
+endclass //environment extends uvm_environment
+
+// ********************* Implementations **********************
+function void alu_environment::build_phase(uvm_phase phase);
+    agnt = alu_agent::type_id::create("agnt", this);
+    scb = alu_scoreboard::type_id::create("scb", this);
+    fc = alu_fun_cov::type_id::create("fc", this);
+endfunction: build_phase
+
+function void alu_environment::connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    agnt.mon.ap.connect(scb.ap_export);
+    agnt.mon.ap.connect(fc.analysis_export);
+endfunction: connect_phase
+
+function void alu_environment::end_of_elaboration_phase(uvm_phase phase);
+    super.end_of_elaboration_phase(phase);
+    scb.set_report_id_action("SCB", UVM_LOG | UVM_DISPLAY);
+endfunction: end_of_elaboration_phase
+
+
+
